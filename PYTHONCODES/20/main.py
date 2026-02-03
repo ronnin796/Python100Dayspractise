05b8c738d2ae21f9707de74ce5f8ac26e32f99fd
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-03 22:18:46 +0545

    Snake completed

--- PYTHONCODES/20/main.py
+++ PYTHONCODES/20/main.py
@@ -38,10 +38,8 @@ while game_on:
         game_on = False
         score.game_over()
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_on = False
             score.game_over()
 
