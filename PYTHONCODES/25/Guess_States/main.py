2f9c8c08585e82ff7e6d6f8afe1c63337c26ba40
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-06 21:58:27 +0545

    Guess the states game

---
+++ PYTHONCODES/25/Guess_States/main.py
@@ -0,0 +1,51 @@
+import turtle
+from pathlib import Path
+import pandas as pd
+
+game_on = True
+BASE_DIR = Path(__file__).parent
+gif_path = str(BASE_DIR / "blank_states_img.gif")
+
+state_data = pd.read_csv(BASE_DIR / "./50_states.csv")
+total_data = len(state_data.state)
+states = list(state_data.state)
+numbers_guessed = 0
+correct_guesses = []
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
+screen = turtle.Screen()
+screen.setup(725, 491)
+screen.tracer(0)
+screen.title("Guess the States")
+
+screen.addshape(gif_path)
+
+
+turtle.shape(gif_path)
+
+
+def get_mouse_click_cor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_cor)
+
+while game_on:
+    screen.update()
+    answer = screen.textinput(
+        title=f"{numbers_guessed}/{total_data}",
+        prompt="Write the state's name: ",
+    )
+    answer = answer.title()
+    if answer in states and answer not in correct_guesses:
+        x_cor = int(state_data["x"][state_data.state == answer].iloc[0])
+        y_cor = int(state_data["y"][state_data.state == answer].iloc[0])
+
+        writer.goto(x_cor, y_cor)
+        writer.write(answer, align="center", font=("Arial", 16, "normal"))
+        numbers_guessed += 1
+        correct_guesses.append(answer)
+
+
+turtle.mainloop()
