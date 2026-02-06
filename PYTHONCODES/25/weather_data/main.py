6ea693d2949f027132c80b9c39a8e6d1d283829a
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-06 18:48:31 +0545

    Pandas operation

--- PYTHONCODES/25/weather_data/main.py
+++ PYTHONCODES/25/weather_data/main.py
@@ -4,6 +4,20 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 
 weather_data = pd.read_csv(BASE_DIR / "./weather_data.csv")
-temperatures = weather_data["temp"]
-print(temperatures)
-print(weather_data)
+temperatures_series = weather_data["temp"]
+temperatures_list = temperatures_series.to_list()
+max_temperature = temperatures_series.max()
+average_temperature = sum(temperatures_list) / len(temperatures_list)
+print(
+    "Average temperature = ",
+    round(average_temperature, 2),
+    " Max temperature = ",
+    max_temperature,
+)
+# Data in Column
+print(weather_data.day)
+# Data in row
+# print(weather_data[weather_data.temp == max_temperature])
+monday_row = weather_data[weather_data.day == "Monday"]
+monday_temperature = monday_row.temp * 9 / 5
+print(monday_temperature)
