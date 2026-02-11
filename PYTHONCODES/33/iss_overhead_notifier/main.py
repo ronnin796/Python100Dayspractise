a17bd31f4b56851c1980f913d167e1737859c21a
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-11 20:09:25 +0545

    ISS notifier and kanye quotes

--- PYTHONCODES/33/iss_overhead_notifier/main.py
+++ PYTHONCODES/33/iss_overhead_notifier/main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 from config import send_notification
+import time
 
 MY_LAT = 28.555429
 MY_LNG = -75.850670
@@ -52,13 +53,11 @@ time_now_hour = time_now.hour
 print(f"Current Hour: {time_now_hour}")
 print(f"Sunrise Hour: {sunrise}")
 print(f"Sunset Hour: {sunset}")
-if is_within_5_degrees(MY_LAT, MY_LNG, iss_latitude, iss_longitude) and (
-    time_now_hour >= sunset or time_now_hour <= sunrise
-):
-    send_notification()
-else:
-    print("ISS is not overhead or it's not dark yet.")
-# If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+while True:
+    time.sleep(60)
+    if is_within_5_degrees(MY_LAT, MY_LNG, iss_latitude, iss_longitude) and (
+        time_now_hour >= sunset or time_now_hour <= sunrise
+    ):
+        send_notification()
+    else:
+        print("ISS is not overhead or it's not dark yet.")
