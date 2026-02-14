fa488fd86d9de466da0aa957d904ede6b81043af
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-14 23:59:45 +0545

    cheapest flight notifications refactored

--- PYTHONCODES/39/flight/flight_search.py
+++ PYTHONCODES/39/flight/flight_search.py
@@ -3,30 +3,14 @@ import requests
 from decouple import config
 from typing import Optional
 from datetime import datetime, timedelta
+from amadeus_connection import AMADEUS
 
 
-class flightSearch:
+class flightSearch(AMADEUS):
 
     def __init__(self):
-        self.api_key = config("AMADEUS_API_KEY")  # from .env
-        self.api_secret = config("AMADEUS_API_SECRET")  # from .env
-        self.token_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        super().__init__()
         self.offer_url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
-        self.headers = {"Authorization": f"Bearer {self.get_token()}"}
-
-    def get_token(self) -> Optional[str]:
-        data = {
-            "grant_type": "client_credentials",
-            "client_id": self.api_key,
-            "client_secret": self.api_secret,
-        }
-        try:
-            response = requests.post(self.token_url, data=data)
-            response.raise_for_status()
-            return response.json().get("access_token")
-        except requests.RequestException as e:
-            logging.error(f"Failed to get Amadeus token: {e}")
-            return None
 
     def search_flight_offers(
         self, origin: str, destination: str, departure_date: str, adults: int = 1
