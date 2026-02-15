518f5fcfeeaa1f2bf32ac2f3d61aa421e2343a7b
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-16 01:30:43 +0545

    Code refactored

--- PYTHONCODES/45/main.py
+++ PYTHONCODES/45/main.py
@@ -1,4 +1,3 @@
-from turtle import hideturtle
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,49 +6,35 @@ class News:
     def __init__(self):
         self.response = requests.get("https://news.ycombinator.com/")
         self.soup = BeautifulSoup(self.response.text, "lxml")
-        self.news_elements = self.soup.find_all(name="tr", class_="athing submission")
-        self.news_list = self.get_news_list()
-
-    def get_news_list(self):
-        self.news_list = [
-            {
-                "id": news.get("id"),
-                "link": news.find("span", class_="titleline").find("a")["href"],
-                "title": news.find("span", class_="titleline").getText(),
-                "upvotes": self.soup.find(
-                    name="span", class_="score", id=f"score_{news.get('id')}"
-                ).getText(),
-            }
-            for news in self.news_elements
+        self.news_elements = self.soup.find_all(name="span", class_="titleline")
+        self.news_upvotes = self.soup.find_all(name="span", class_="score")
+        self.news_link = [news.find("a")["href"] for news in self.news_elements]
+        self.news_title = [news.getText() for news in self.news_elements]
+        self.news_upvotes = [
+            int(news.getText().strip(" points")) for news in self.news_upvotes
         ]
-        return self.news_list
 
     def get_highest_upvoted_news(self):
-        highest_upvotes = 0
-        highest_upvotes_index = 0
-        for index, news in enumerate(self.news_list):
-            if int(news["upvotes"].strip(" points")) > highest_upvotes:
-                highest_upvotes = int(news["upvotes"].strip(" points"))
-                highest_upvotes_index = index
-        return self.news_list[highest_upvotes_index]
+        highest_upvoted_news_index = self.news_upvotes.index(max(self.news_upvotes))
+        return (
+            self.news_title[highest_upvoted_news_index],
+            self.news_link[highest_upvoted_news_index],
+            self.news_upvotes[highest_upvoted_news_index],
+        )
 
-    def print_news_list(self):
-        for news in self.news_list:
-            print(news["title"])
-            print(news["link"])
-            print(news["upvotes"])
-            print(news["id"])
+    def print_all_news(self):
+        for title, link, upvotes in zip(
+            self.news_title, self.news_link, self.news_upvotes
+        ):
+            print(f"The news is: {title}")
+            print(f"The link to the news is: {link}")
+            print(f"The upvotes are: {upvotes}")
             print("--------------------------------")
-
-    def print_highest_upvoted_news(self):
-        highest_upvoted_news = self.get_highest_upvoted_news()
-        print(f"The most upvoted news is: {highest_upvoted_news['title']}")
-        print(f"The link to the news is: {highest_upvoted_news['link']}")
-        print(f"The upvotes are: {highest_upvoted_news['upvotes']}")
-        print(f"The id is: {highest_upvoted_news['id']}")
-        print("--------------------------------")
 
 
 news = News()
-news.print_news_list()
-news.print_highest_upvoted_news()
+TITLE, LINK, UPVOTES = news.get_highest_upvoted_news()
+print(f"The most upvoted news is: {TITLE}")
+print(f"The link to the news is: {LINK}")
+print(f"The upvotes are: {UPVOTES}")
+news.print_all_news()
