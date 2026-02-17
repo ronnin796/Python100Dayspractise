751015e65978f149e8d0819aaa45b0bef30aca2f
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-17 23:56:13 +0545

    Get price and send email if price dropped

--- PYTHONCODES/47/main.py
+++ PYTHONCODES/47/main.py
@@ -1,5 +1,7 @@
+from email import message
 from bs4 import BeautifulSoup
 import requests
+from sendmail import send_mail
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
@@ -17,13 +19,43 @@ amazon_url = (
     "https://www.amazon.com/Lenovo-Legion-Gaming-GeForce-Windows/dp/B0FL4HLJ56/"
 )
 
-response = requests.get(url=amazon_url, headers=headers)
 
-soup = BeautifulSoup(response.content, "html.parser")
-print(soup.prettify())
-# Find the price
-price_tag = soup.find("span", class_="a-price-whole")
-if price_tag:
-    print("Price:", price_tag.get_text())
-else:
-    print("Price not found. Amazon may be blocking requests.")
+class Amazon_price:
+    def __init__(self, headers=headers, url=amazon_url) -> None:
+        self.url = url
+        self.headers = headers
+
+    def scrape_site(self) -> [any]:
+        return requests.get(url=self.url, headers=self.headers)
+
+    def get_title(self):
+        response = requests.get(url=self.url, headers=self.headers)
+        soup = self.soupify(response)
+        return soup.find("span", id="productTitle").get_text(strip=True)
+
+    def fetch_price(
+        self, tag: str = "span", class_: str = "a-price-whole", *args: str
+    ) -> str:
+        response = self.scrape_site()
+        soup = self.soupify(response)
+        price_tag = soup.find(tag, class_=class_)
+        if price_tag:
+            return price_tag.get_text()
+        else:
+            print("Price not found. Amazon may be blocking requests.")
+
+    def soupify(self, response, parser="html.parser") -> any:
+        return BeautifulSoup(response.content, parser)
+
+
+amz = Amazon_price()
+price_str = amz.fetch_price()  # e.g., "1,299.50"
+price_float = float(price_str.replace(",", ""))
+acceptable_price = 2500
+title = amz.get_title()
+
+if price_float <= acceptable_price:
+    message = (
+        f"the price of {title} has been dropped. Its current price is {price_float}"
+    )
+    send_mail("Price dropped ", message)
