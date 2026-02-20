a9eee949f49cd1266beedd1ccf9f7f6999b4d573
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-20 21:05:07 +0545

    auto login

--- PYTHONCODES/49/main.py
+++ PYTHONCODES/49/main.py
@@ -1,18 +1,35 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import time
+
+ACCOUNT_EMAIL = "noctis@gmail.com"
+ACCOUNT_PASSWORD = "nocits1234"
 
 options = Options()
-# options.add_argument("--headless=new")
+options.add_experimental_option("detach", True)
 options.binary_location = "/usr/bin/brave-browser"
+options.add_argument(
+    "--user-data-dir=/home/ronninx/.config/BraveSoftware/Brave-Browser"
+)
+options.add_argument("--profile-directory=Profile 1")
 
 driver = webdriver.Chrome(options=options)
-
 driver.get("https://appbrewery.github.io/gym/")
+
 wait = WebDriverWait(driver, 10)
-driver.quit()
+
+# Click login button first (if needed)
+login_button = wait.until(EC.element_to_be_clickable((By.ID, "login-button")))
+login_button.click()
+
+# Wait for form fields
+email_field = wait.until(EC.presence_of_element_located((By.ID, "email-input")))
+password_field = wait.until(EC.presence_of_element_located((By.ID, "password-input")))
+submit_button = wait.until(EC.element_to_be_clickable((By.ID, "submit-button")))
+
+# Login
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field.send_keys(ACCOUNT_PASSWORD)
+submit_button.click()
