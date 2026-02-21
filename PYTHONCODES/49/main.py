6b6efe0099d0f43fb228fa1bba4a7d5cb7f27951
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-21 21:10:41 +0545

    auto book tuesday

--- PYTHONCODES/49/main.py
+++ PYTHONCODES/49/main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ElementClickInterceptedException
 
 ACCOUNT_EMAIL = "noctis@gmail.com"
 ACCOUNT_PASSWORD = "nocits1234"
@@ -33,3 +35,32 @@ submit_button = wait.until(EC.element_to_be_clickable((By.ID, "submit-button")))
 email_field.send_keys(ACCOUNT_EMAIL)
 password_field.send_keys(ACCOUNT_PASSWORD)
 submit_button.click()
+
+
+# Book tuesday classes
+def book_tuesday():
+    # Wait for at least one Tuesday available button
+    wait.until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "//h2[contains(@id,'day-title-tue')]/following-sibling::div//button[contains(@class,'ClassCard_available__')]",
+            )
+        )
+    )
+
+    book_buttons = driver.find_elements(
+        By.XPATH,
+        "//h2[contains(@id,'day-title-tue')]/following-sibling::div//button[contains(@class,'ClassCard_available__')]",
+    )
+
+    for button in book_buttons:
+        try:
+            driver.execute_script("arguments[0].scrollIntoView(true);", button)
+            wait.until(EC.element_to_be_clickable(button))
+            button.click()
+        except ElementClickInterceptedException:
+            driver.execute_script("arguments[0].click();", button)
+
+
+book_tuesday()
